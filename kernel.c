30d32e700fd3f9997cfb1368eae7e79ede607e37
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-10-25 01:31:16 +0900

    add kernel panic

--- kernel.c
+++ kernel.c
@@ -27,11 +27,16 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
+  memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
   printf("%");
   printf("%%");
   printf("\n\nHello World!\n");
   printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
   printf("1 + 2 = %d, %X\n", 1 + 2, 0x1234abcd);
+
+  PANIC("booted!");
+  printf("unreachable here!\n");
 
   for (;;) {
     __asm__ __volatile__("wfi");
