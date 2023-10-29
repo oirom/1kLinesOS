9543a9fe1f1c8efdd5884cdfe47dc94a09aaf023
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-10-29 23:09:10 +0900

    add process

--- kernel.h
+++ kernel.h
@@ -59,3 +59,14 @@ struct sbiret {
   long error;
   long value;
 };
+
+#define PROCS_MAX     8
+#define PROC_UNUSED   0
+#define PROC_RUNNABLE 1
+
+struct process {
+  int pid;
+  int state;
+  vaddr_t sp;
+  uint8_t stack[8192];
+};
