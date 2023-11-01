6fe11fa45aba07553bca93a20f97365cddb2c844
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-11-01 22:59:47 +0900

    add page table

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include "kernel.h"
 #include "common.h"
 
+extern char __kernel_base[];
 extern char __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 extern char __bss[], __bss_end[];
@@ -19,6 +20,24 @@ paddr_t alloc_pages(uint32_t n) {
 
   memset((void *) paddr, 0, n * PAGE_SIZE);
   return paddr;
+}
+
+void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags) {
+  if (!is_aligned(vaddr, PAGE_SIZE))
+    PANIC("unsligned vaddr %x", vaddr);
+
+  if (!is_aligned(paddr, PAGE_SIZE))
+    PANIC("unsligned paddr %x", paddr);
+
+  uint32_t vpn1 = (vaddr >> 22) & 0x3ff;
+  if ((table1[vpn1] & PAGE_V) == 0) {
+    uint32_t pt_paddr = alloc_pages(1);
+    table1[vpn1] = ((pt_paddr / PAGE_SIZE) << 10) | PAGE_V;
+  }
+
+  uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
+  uint32_t *table0 = (uint32_t *) ((table1[vpn1] >> 10) * PAGE_SIZE);
+  table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
 }
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
@@ -191,9 +210,17 @@ struct process *create_process(uint32_t pc) {
   *--sp = 0;              // s0
   *--sp = (uint32_t) pc;  // ra
 
+  uint32_t *page_table = (uint32_t *) alloc_pages(1);
+
+  for (paddr_t paddr = (paddr_t) __kernel_base;
+       paddr < (paddr_t) __free_ram_end;
+       paddr += PAGE_SIZE)
+    map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+
   proc->pid = i + 1;
   proc->state = PROC_RUNNABLE;
   proc->sp = (uint32_t) sp;
+  proc->page_table = page_table;
   return proc;
 }
 
@@ -211,9 +238,13 @@ void yield(void) {
     return;
 
   __asm__ __volatile__(
+    "sfence.vma\n"
+    "csrw satp, %[satp]\n"
+    "sfence.vma\n"
     "csrw sscratch, %[sscratch]\n"
     :
-    : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+    : [satp] "r" (SATP_SV32 | ((uint32_t) next->page_table / PAGE_SIZE)),
+      [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
   );
 
   struct process *prev = current_proc;
