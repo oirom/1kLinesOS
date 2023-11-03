53fc08de100f561d99f52ad348568899a7773ab4
oirom <yamauchi.morio853@mail.kyutech.jp>
2023-11-04 00:09:39 +0900

    add shell command

--- shell.c
+++ shell.c
@@ -1,7 +1,28 @@
 #include "user.h"
 
 void main(void) {
-  // page fault
-  // *((volatile int *) 0x80200000) = 0x1234;
-  printf("Hello World from shell!\n");
+  while (1) {
+prompt:
+    printf("> ");
+    char cmdline[128];
+    for (int i = 0;; i++) {
+      char ch = getchar();
+      putchar(ch);
+      if (i == sizeof(cmdline) - 1) {
+        printf("commnad line too long\n");
+        goto prompt;
+      } else if (ch == '\r') {
+        printf("\n");
+        cmdline[i] = '\0';
+        break;
+      } else {
+        cmdline[i] = ch;
+      }
+    }
+
+    if (strcmp(cmdline, "hello") == 0)
+      printf("Hello world from shell!\n");
+    else
+      printf("unknown command: %s\n", cmdline);
+  }
 }
